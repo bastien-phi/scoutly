efdf150151b55df32a1b5b5a2deda0ff8fabcf4d
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-05-12 09:36:15 +0200

    feat(Models): configure strict models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+    }
+
+    protected function configureModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict(!app()->isProduction());
+
+        Relation::enforceMorphMap([]);
     }
 }
