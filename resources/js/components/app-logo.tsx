48e6b6613096f0bbb486162b90e171d1216a6fe8
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-05-14 09:56:55 +0200

    feat(logo): use AppLogo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,8 @@
-import AppLogoIcon from './app-logo-icon';
+import { SVGAttributes } from 'react';
+import ScoutlyBanner from '@assets/scoutly-banner.svg?react';
 
-export default function AppLogo() {
+export default function AppLogo(props: SVGAttributes<SVGElement>) {
     return (
-        <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
-            </div>
-        </>
+        <ScoutlyBanner {...props} height={undefined} width={undefined} />
     );
 }
