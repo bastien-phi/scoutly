7ebb9f61ef857d958a715deae45d6ff26864773b
bastien-phi <10199039+bastien-phi@users.noreply.github.com>
2025-05-15 06:28:29 +0000

    cs: fix cs

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,8 +1,6 @@
-import { SVGAttributes } from 'react';
 import Scoutly from '@assets/scoutly.svg?react';
+import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <Scoutly {...props} />
-    );
+    return <Scoutly {...props} />;
 }
--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,8 +1,6 @@
-import { SVGAttributes } from 'react';
 import ScoutlyBanner from '@assets/scoutly-banner.svg?react';
+import { SVGAttributes } from 'react';
 
 export default function AppLogo(props: SVGAttributes<SVGElement>) {
-    return (
-        <ScoutlyBanner {...props} height={undefined} width={undefined} />
-    );
+    return <ScoutlyBanner {...props} height={undefined} width={undefined} />;
 }
