3849816890dcc20e4e7658e61f3134dfd981e775
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-06-12 09:36:43 +0200

    wip

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -10,3 +10,13 @@ export function cn(...inputs: ClassValue[]) {
 export function routeMatches<T extends ValidRouteName>(page: Page, name: T, params?: RouteParams<T> | undefined, config?: Config): boolean {
     return page.url.startsWith(route(name, params, false, config));
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<F extends (...params: any[]) => ReturnType<F>>(fn: F, delay: number): F {
+    let timeoutID: NodeJS.Timeout;
+    return function (...args: unknown[]) {
+        clearTimeout(timeoutID);
+        // @ts-expect-error - TypeScript doesn't know `this` type
+        timeoutID = setTimeout(() => fn.apply(this, args), delay);
+    } as F;
+}
