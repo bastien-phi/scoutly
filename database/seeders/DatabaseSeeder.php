e481129b3b2c0fbd472054e07d68d0821330dfbc
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-06-16 10:56:41 +0200

    Update seeder to have tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Link;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // User::factory(10)->create();
+        $tags = Tag::factory(10)->create();
 
-        User::factory()
-            ->hasLinks(20)
-            ->create([
-                'name' => 'Test User',
-                'username' => 'testuser',
-                'email' => 'test@example.com',
-            ]);
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'username' => 'testuser',
+            'email' => 'test@example.com',
+        ]);
+
+        Collection::times(
+            20,
+            fn () => Link::factory()
+                ->for($user)
+                ->recycle($tags)
+                ->hasAttached(
+                    $tags->random(
+                        fake()->optional(weight: 0.8, default: 0)
+                            ->numberBetween(1, 5)
+                    )
+                )
+                ->createOne()
+        );
     }
 }
