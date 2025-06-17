cff15826b0d563a9ee9376f40602070ee0877129
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-06-17 14:03:40 +0200

    Customize emails

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ url('scoutly-banner.png') }}" class="logo" alt="Scoutly Logo">
+</a>
+</td>
+</tr>
