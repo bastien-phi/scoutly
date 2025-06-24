ba094e107bbbf030620316de28d2fd0251f1ddf4
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-06-24 08:43:55 +0200

    Change author name and tag label to ci text

---
+++ database/migrations/2025_06_24_063355_create_extension_citext.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::createExtensionIfNotExists('citext');
+    }
+
+    public function down(): void
+    {
+        Schema::dropExtensionIfExists('citext');
+    }
+};
