d683658b87f0bf6504242c3d6896dd5b4ad72f4e
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-06-25 09:39:55 +0200

    Refactor community routes definition, use ziggy strict route names

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -3,3 +3,9 @@ import type { route as routeFn } from 'ziggy-js';
 declare global {
     const route: typeof routeFn;
 }
+
+declare module 'ziggy-js' {
+    interface TypeConfig {
+        strictRouteNames: true;
+    }
+}
