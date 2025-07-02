cda291e755c0c4c44c8ef2931a7c90e5aee0e0fd
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-07-02 15:28:48 +0200

    Use rolldown-vite and vitejs/plugin-react-oxc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-oxc';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
@@ -32,9 +32,6 @@ export default defineConfig({
             }
         ]),
     ],
-    esbuild: {
-        jsx: 'automatic',
-    },
     resolve: {
         alias: {
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
